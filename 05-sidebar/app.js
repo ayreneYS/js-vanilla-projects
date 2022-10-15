19c09de74aba2d862a617c82e169d873e649b642
Ayrene <112762086+ayreneYS@users.noreply.github.com>
2022-10-15 20:07:45 +0800

    js-vanilla-projects

---
+++ 05-sidebar/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const toggleBtn = document.querySelector('.sidebar-toggle');
+const closeBtn = document.querySelector('.close-btn');
+const sidebar = document.querySelector('.sidebar');
+
+toggleBtn.addEventListener('click', function () {
+  sidebar.classList.toggle('show-sidebar');
+});
+
+closeBtn.addEventListener('click', function () {
+  sidebar.classList.remove('show-sidebar');
+});
